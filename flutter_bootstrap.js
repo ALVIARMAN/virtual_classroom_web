3b9c698a6d9aac3de756ec2154d105c38798ab02
alviarman009@gmail.com <alviarman@alviarman009gmailcoms-MacBook-Air.local>
2026-08-17 13:17:34 +0600

    Add section all classroom

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"035316565ad77281a75305515e4682e6c4c6f7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2283460224"
+    serviceWorkerVersion: "3360820307"
   }
 });
