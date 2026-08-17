a1d44d5dfad186b451b501487b9fef9b0176ea6a
alviarman009@gmail.com <alviarman@alviarman009gmailcoms-MacBook-Air.local>
2026-08-17 14:46:31 +0600

    all logo

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"035316565ad77281a75305515e4682e6c4c6f7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4041080213"
+    serviceWorkerVersion: "3911429030"
   }
 });
