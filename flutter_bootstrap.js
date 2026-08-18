464732df22b083ce2ae635c653c46ea031ab5224
alviarman009@gmail.com <alviarman@alviarman009gmailcoms-MacBook-Air.local>
2026-08-19 04:10:15 +0600

    all classes can be seen

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"035316565ad77281a75305515e4682e6c4c6f7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1898915769"
+    serviceWorkerVersion: "286163819"
   }
 });
